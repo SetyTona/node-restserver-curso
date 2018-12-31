7f451ad06bfc52f5b7f6fa36aa506a13739eb494
SetyTona <setytona@gmail.com>
2018-12-31 08:11:24 +0100

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -26,7 +26,18 @@ if (process.env.NODE_ENV === 'dev') {
     // Si no estamos en desarrollo, cogemos la ruta de nuestro server en la nube
     // 'mongodb://<user>:<password>@ds145474.mlab.com:45474/cafe';
     // <user> y <password> : obtenidos de mLab, usuarios con acceso permitido a la BBDD cafe
-    urlBBDD = 'mongodb://cafe-user:cafe123456@ds145474.mlab.com:45474/cafe';
+    // urlBBDD = 'mongodb://cafe-user:cafe123456@ds145474.mlab.com:45474/cafe';
+
+    // Hemos ocultado esta cadena de conexión para que cuando lo subamos a GITHUB, no esté disponible para los demás 
+    // usuarios, ya que en ella, aparece el nombre de usuario con acceso a la BBDD y su password.
+    // Para ocultarlo, creamos una variable de entorno en HEROKU
+    // para eso, lo que hacemos es desde el directorio del aplicativo, hacer lo siguiente:
+    //  heroku config:set MONGO_URI="mongodb://cafe-user:cafe123456@ds145474.mlab.com:45474/cafe"
+    // con esto, crearemos la variable de proceso MONGO_URI
+    // y luego substituimos, la cadena de conexión por la variable MONGO_URI
+
+    urlBBDD = process.env.MONGO_URI;
+
 }
 
 // Esto lo hacemos para compartir la variable en otros ficheros
